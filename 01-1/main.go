9a6a136747fc390532bfc9fda18d5dc5d5673360
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2026-09-12 19:11:19 +0200

    01-1 solution

--- 01-1/main.go
+++ 01-1/main.go
@@ -3,18 +3,51 @@ package main
 import (
   "bufio"
   "fmt"
+  "log"
+  "strconv"
+  "strings"
   "os"
 )
 
 func main() {
   reader := bufio.NewReader(os.Stdin)
+  position := 50
+  modulus := 100
+  zeroes := 0
   for {
     line, err := reader.ReadString('\n')
-    if err != nil {
+    line = strings.TrimSpace(line)
+    if err != nil || len(line) == 0  {
       break
     }
-    fmt.Println(line)
+    orientation := line[0:1]
+    amount, err2  := strconv.Atoi(line[1:])
+    if err2 != nil {
+      log.Fatal(err2)
+    }
+    // fmt.Println(orientation, amount)
+    switch orientation {
+    case "L":
+      amount = -amount
+    case "R":
+    default:
+      log.Fatal("Unknown direction", orientation)
+    }
+
+    // Make amount positive in the 0-modulus range
+    amount = amount % modulus
+    amount += modulus
+
+    // Add to the current position, also keeping within the 0-modulus range
+    position += amount
+    position = position % modulus
+    // fmt.Println(position)
+
+    if position == 0 {
+      zeroes++
+    }
+
   }
-  fmt.Println("Done")
+  fmt.Println("Done", zeroes)
 }
 
