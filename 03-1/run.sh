e1c056e610410d3e529652392e24bbbb2812a7c1
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2026-09-19 21:03:54 +0200

    Prevent warning message due to go.mod file exists

--- 03-1/run.sh
+++ 03-1/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-go mod init AdventOfCode/`basename "${PWD}"`
+[ ! -f go.mod ] && go mod init AdventOfCode/`basename "${PWD}"`r
 echo "Example" && cat example.txt | go run .
 echo "Input" && cat input.txt | go run .
--- 03-2/run.sh
+++ 03-2/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-go mod init AdventOfCode/`basename "${PWD}"`
+[ ! -f go.mod ] && go mod init AdventOfCode/`basename "${PWD}"`r
 echo "Example" && cat ../03-1/example.txt | go run .
 echo "Input" && cat ../03-1/input.txt | go run .
