d58bf62fb8e8d5e63f86819accf90306741f13a5
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2026-09-22 22:56:22 +0200

    Day 4 part 2

---
+++ 04-2/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[ ! -f go.mod ] && go mod init AdventOfCode/`basename "${PWD}"`r
+echo "Example" && cat ../04-1/example.txt | go run .
+echo "Input" && cat ../04-1/input.txt | go run .
